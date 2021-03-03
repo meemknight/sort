35783b25e115a98b071f7b7c6e876b93e0155673
vlod <vladalex420@gmail.com>
2021-03-03 14:08:10 +0200

    setup project

---
+++ sources/main.cpp
@@ -0,0 +1,35 @@
+#include <glad/glad.h>
+#include <iostream>
+#include <ctime>
+#include "tools.h"
+
+#ifdef PLATFORM_WIN32
+#include <Windows.h>
+#endif
+
+#undef min
+#undef max
+
+
+int main()
+{
+
+#ifdef PLATFORM_WIN32
+#ifdef _MSC_VER 
+	AllocConsole();
+	freopen("conin$", "r", stdin);
+	freopen("conout$", "w", stdout);
+	freopen("conout$", "w", stderr);
+	std::cout.sync_with_stdio();
+#endif
+#endif
+
+
+	std::cout << "test";
+
+
+
+
+	std::cin.clear();
+	std::cin.get();
+}
